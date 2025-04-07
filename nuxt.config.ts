acad6c467ebfca0021ceb9cf1a5c3f100380db39
OlivierRecher <recher.olivier@outlook.fr>
2025-04-07 11:52:47 +0200

    feat(): add more icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,11 @@ export default defineNuxtConfig({
       theme_color: '#efb100',
       icons: [
         {
+          src: '/favicon-96x96.png',
+          sizes: '96x96',
+          type: 'image/png',
+        },
+        {
           src: '/icon-192.png',
           sizes: '192x192',
           type: 'image/png',
